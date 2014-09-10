e1a092f83f0df577c15d45770ae39ef7bed2e63a
Larry Davis <lazdnet@gmail.com>
2014-09-10 11:02:47 -0700

    Tweak gravity, reorder code, use upper case for constants, add background, add music

--- vectorman.js
+++ vectorman.js
@@ -1,16 +1,29 @@
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, 'vectorman', {
+var game = new Phaser.Game(1024, 768, Phaser.AUTO, 'vectorman', {
   preload: preload,
   create: create,
   update: update,
   render: render
 });
 
-function preload() {
-  game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.image('tiles', 'assets/tiles.png');
-  game.load.spritesheet('vectorman', 'assets/vectorman.png', 100, 100);
-  game.load.image('background', 'assets/background2.png');
-}
+var ANIMATIONSPEED = 60; // fps
+
+// The number of frames in the landing animation
+var TIMETOLAND = 16/60 * 1000;
+
+// The number of frames in the start running animation
+var TIMETORUN = 26/60 * 1000;
+
+// World gravity
+var GRAVITY = 650;
+
+// The time between jumps
+var JUMPVELOCITY = 325;
+var BOOSTVELOCITY = 350;
+var MAXBOOSTS = 1;
+
+// The speed of running
+var RUNSTARTSPEED = 180;
+var RUNFULLSPEED = 300;
 
 var map;
 var tileset;
@@ -24,9 +37,19 @@ var runTimer = 0;
 var cursors;
 var jumpButton;
 var bg;
+var music;
 
-// 60 FPS
-var spriteSpeed = 60;
+// Whether the player is currently running at full speed
+var fullSpeed = false;
+
+// The time when we can next boost
+var jumpReleased = true;
+
+// The number of boosts the player has used in flight
+var boostCount = 0;
+
+// When the player started landing
+var landStart = 0;
 
 // 1 based frame numbers of animations
 var animations = {
@@ -39,6 +62,17 @@ var animations = {
   'land': [157, 173, false]
 };
 
+function preload() {
+  game.load.tilemap('level1', 'assets/level1.json', null, Phaser.Tilemap.TILED_JSON);
+  game.load.image('tiles', 'assets/tiles.png');
+  game.load.spritesheet('vectorman', 'assets/vectorman.png', 100, 100);
+  game.load.image('background', 'assets/background.png');
+  game.load.audio('bamboo-mill', ['assets/music/Bamboo Mill.ogg']);
+}
+
+/**
+  Create an array with numbers from start to end inclusive
+*/
 function spriteMap(start, end) {
   var map = [];
   for (var i = start; i <= end; i++) {
@@ -48,12 +82,11 @@ function spriteMap(start, end) {
 }
 
 function create() {
-
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  game.stage.backgroundColor = '#000000';
-
-  bg = game.add.tileSprite(0, 0, 800, 600, 'background');
+  // Same as bottom of background
+  game.stage.backgroundColor = '#261e11';
+  bg = game.add.tileSprite(0, 0, 3188, 900, 'background');
   bg.fixedToCamera = true;
 
   map = game.add.tilemap('level1');
@@ -69,21 +102,23 @@ function create() {
   // Collide on everything in the foreground
   map.setCollisionBetween(0, 1000, true, foreground);
 
-  game.physics.arcade.gravity.y = 500;
+  game.physics.arcade.gravity.y = GRAVITY;
 
   player = game.add.sprite(128, 768, 'vectorman');
-  player.anchor.setTo(0.5, 0); //so it flips around its middle
+  player.anchor.setTo(0.5, 0); // So it flips around its middle
   game.physics.enable(player, Phaser.Physics.ARCADE);
 
-  player.body.bounce.y = 0; // No bounce!
+  player.body.bounce.y = 0; // Don't bounce
   player.body.collideWorldBounds = true;
   player.body.setSize(50, 50, 0, 39);
+
+  setSpriteDirection(facing);
 
   // Setup animations
   for (var animationName in animations) {
     var animationFrames = animations[animationName];
     console.log('Animation %s runs from %d to %d', animationName, animationFrames[0]-1, animationFrames[1]-1);
-    player.animations.add(animationName, spriteMap(animationFrames[0]-1, animationFrames[1]-1), spriteSpeed, animationFrames[2]);
+    player.animations.add(animationName, spriteMap(animationFrames[0]-1, animationFrames[1]-1), ANIMATIONSPEED, animationFrames[2]);
   }
 
   game.camera.follow(player);
@@ -91,49 +126,34 @@ function create() {
   // Controls
   cursors = game.input.keyboard.createCursorKeys();
   jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+  // Play music
+  music = game.add.audio('bamboo-mill');
+  music.play();
 }
 
-// The time it takes to start running full speed
-var timeToRun = 26/60 * 1000; // The number of frames in the start running animation
-
-var landStart = 0;
-var timeToLand = 16/60 * 1000;
-
-// The time between jumps
-var jumpTime = 250;
-var jumpVelocity = 275;
-var boostVelocity = 300;
-var maxBoosts = 1;
-
-// The number of boosts the player has used in flight
-var boostCount = 2;
-
-// The speed of running
-var runStartSpeed = 180;
-var runFullSpeed = 300;
-
-var fullSpeed = false;
-
-// The time when we can next boost
-var jumpStart = 0;
-var lastBoostTime = 0;
-var jumpReleased = true;
-
-function run(direction) {
-  var additionalRunVelocity = 0;
-
-  // Set sprite direction
+/**
+  Flip the sprite to match the given direction
+*/
+function setSpriteDirection(direction) {
   if (direction === 'right') {
     player.scale.x = -1;
   }
   else {
     player.scale.x = 1;
   }
+}
+
+function run(direction) {
+  var additionalRunVelocity = 0;
+
+  // Set sprite direction
+  setSpriteDirection(direction);
 
   // If they switch direction or just start running
   if (!running) {
     // Start a timer where we'll run at full speed
-    runTimer = game.time.now + timeToRun;
+    runTimer = game.time.now + TIMETORUN;
 
     fullSpeed = false;
     running = true;
@@ -148,7 +168,7 @@ function run(direction) {
 
   if (!fullSpeed) {
     // Accerate to full speed
-    additionalRunVelocity = (1 - (runTimer - game.time.now) / timeToRun) * (runFullSpeed - runStartSpeed);
+    additionalRunVelocity = (1 - (runTimer - game.time.now) / TIMETORUN) * (RUNFULLSPEED - RUNSTARTSPEED);
   }
 
   if (player.body.onFloor()) {
@@ -163,7 +183,7 @@ function run(direction) {
   }
 
   // Set speed
-  player.body.velocity.x = (direction === 'left' ? -1 : 1) * ((fullSpeed ? runFullSpeed : runStartSpeed) + additionalRunVelocity);
+  player.body.velocity.x = (direction === 'left' ? -1 : 1) * ((fullSpeed ? RUNFULLSPEED : RUNSTARTSPEED) + additionalRunVelocity);
 }
 
 function update() {
@@ -190,7 +210,7 @@ function update() {
   else if (cursors.right.isDown) {
     run('right');
   }
-  else if (mode != 'land' || game.time.now > landStart + timeToLand) {
+  else if (mode != 'land' || game.time.now > landStart + TIMETOLAND) {
     if (onFloor) {
       player.animations.play('idle');
       mode = 'idle';
@@ -206,20 +226,19 @@ function update() {
     // not pressing jump
     // not over boost count
   var canBoost = false;
-  if (jumpReleased && !onFloor && boostCount < maxBoosts) {
+  if (jumpReleased && !onFloor && boostCount < MAXBOOSTS) {
     canBoost = true;
   }
 
   if (jumpButton.isDown) {
     if (jumpReleased) {
       if (onFloor) {
-        player.body.velocity.y = -1 * jumpVelocity;
-        jumpStart = game.time.now;
+        player.body.velocity.y = -1 * JUMPVELOCITY;
         mode = 'jump';
         player.animations.play('jump');
       }
       else if (jumpButton.isDown && canBoost) {
-        player.body.velocity.y = -1 * boostVelocity;
+        player.body.velocity.y = -1 * BOOSTVELOCITY;
         boostCount++;
         player.animations.stop();
         player.animations.play('boost');
@@ -238,7 +257,7 @@ function update() {
   }
 }
 
-function render () {
+function render() {
   // game.debug.text(game.time.physicsElapsed, 32, 32);
   // game.debug.body(player);
   // game.debug.bodyInfo(player, 16, 24);
